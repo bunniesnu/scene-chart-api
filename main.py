80b6209fc76ddab290e1aa7ec5651e6a0bd57f09
Bunniesnu <bunniesnu@gmail.com>
2026-09-01 00:58:18 +0900

    Add archive_stream_reports job

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from melon import MelonClient
 from sqlmodel import Session
 
 from archive.artist import archive_artist
-from archive.chart import archive_charts, archive_charts_midnight
+from archive.chart import archive_charts, archive_charts_midnight, archive_stream_reports
 from src.const import ARTIST_ID
 from src.db.db import engine
 from src.utils.webhook import send_something_went_wrong
@@ -37,6 +37,16 @@ def run_archive_charts_midnight():
 
         with Session(engine) as session:
             archive_charts_midnight(session, client, ARTIST_ID)
+    except Exception as e:
+        send_something_went_wrong(e)
+        return traceback.format_exc()
+
+def run_archive_stream_reports():
+    try:
+        client = MelonClient()
+
+        with Session(engine) as session:
+            archive_stream_reports(session, client, ARTIST_ID)
     except Exception as e:
         send_something_went_wrong(e)
         return traceback.format_exc()
