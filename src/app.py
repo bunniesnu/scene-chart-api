2d2c10a3f7cba471cb74c43e14cfeb95cb0e6e73
Bunniesnu <bunniesnu@gmail.com>
2026-09-02 06:45:43 +0900

    Add FRONTEND_HOSTS env for CORS

--- src/app.py
+++ src/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from os import getenv
 
 from src.routes import artist, charts
 
 app = FastAPI()
+
+origins = getenv("FRONTEND_HOSTS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(artist.router)
 app.include_router(charts.router)
